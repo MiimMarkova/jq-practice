34e6927306af689b72bc2d53a6d9632b6d5be5e3
MiimMarkova <mariapmarkova@abv.bg>
2016-08-12 15:32:07 +0300

    Add files via upload

---
+++ server.php
@@ -0,0 +1,14 @@
+<?php
+
+const USERNAME = mimi;
+const PASSWORD = mimi23;
+
+
+$userName = isset($_POST['logName']) ? $_POST['logName'] : '';
+$password = isset($_POST['logPassword']) ? $_POST['logPassword'] : '';
+
+if ($userName == USERNAME){
+	
+}
+
+echo $userName;
